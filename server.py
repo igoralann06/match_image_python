6b669be26b6985b89264fc95cda543997f65af2b
Igor <igoralann113@gmail.com>
2025-03-25 11:17:44 +0900

    updated

--- server.py
+++ server.py
@@ -56,7 +56,7 @@ def find_similar_images(uploaded_image_path):
     
     file_input = driver.find_element(By.CSS_SELECTOR, "input[type='file']")
     file_input.send_keys(os.path.abspath(uploaded_image_path))
-    print(uploaded_image_path)
+    print(os.path.abspath(uploaded_image_path))
     time.sleep(5)
     
     results = driver.find_elements(By.TAG_NAME, "img")
